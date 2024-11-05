37b9a9b6abc588e5555815c41e7f2d5dc6c4fab5
Pabl09 <munozjuanpablo424@gmail.com>
2024-11-05 14:29:47 -0600

    Se actualiza el diseño de las Skills

--- script.js
+++ script.js
@@ -15,30 +15,6 @@ function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
-//Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("AndroidStudio");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("Database");
-        habilidades[3].classList.add("React");
-        habilidades[4].classList.add("Figma");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
-    }
-}
-
-
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
-window.onscroll = function(){
-    efectoHabilidades();
-} 
 
 // Funcion para visualizar y descargar CV en formato PDF
 document.getElementById('download-btn').addEventListener('click', function() {
